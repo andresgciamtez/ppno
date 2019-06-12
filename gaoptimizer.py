1f6cdf52d07f11d1054271572202c7a4b3a82425
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2019-06-12 19:11:46 +0200

    new algorithm nsga2

---
+++ gaoptimizer.py
@@ -0,0 +1,98 @@
+﻿# -*- coding: utf-8 -*-
+"""PRESSURIZED PIPE NETWORK OPTIMIZING BY PYGMO
+Andrés García Martínez (agarciam@citop.es)
+Licensed under the Apache License 2.0. http://www.apache.org/licenses/
+"""
+
+from time import perf_counter
+import numpy as np
+import pygmo as pg
+
+# PARAMETERS
+GENERATIONSPERTRIAL = 100
+POPULATIONSIZE = 100
+MAXTRIALS = 250
+MAXNOCHANGES = 10
+MAXTIME = 10*60
+
+class PPNOProblem():        
+    def __init__(self, ppn):
+        self.mynet = ppn
+            
+    def fitness(self, x):
+        tmp = np.array(x, dtype=np.int)
+        self.mynet.set_x(tmp)
+        f1 = float(self.mynet.get_cost())
+        f2 = float(max(self.mynet.check(mode='PD')))
+        return [f1, f2]
+    
+    def get_bounds(self):
+        lb = self.mynet.lbound
+        ub = self.mynet.ubound
+        return (lb, ub)
+    
+    def get_nobj(self):
+        return 2
+    
+    def get_nix(self):
+        return self.mynet.dimension
+   
+    def get_name(self):
+        return "Pressurized Pipe Network Optimization problem"   
+    
+def nsga2(ppn):
+    print('*** NSGA-II ALGORITHM ***')
+    # INSTANCIATE A PYGMO PROBLEM CONSTRUCTING IT FROM A UDP
+    inittime = perf_counter()
+    prob = pg.problem(PPNOProblem(ppn))
+    generations = trials = nochanges = 0
+    best_f = best_x =  None 
+    # INSTANCIATE THE PYGMO ALGORITM NSGA-II
+    algo = pg.algorithm(pg.nsga2(gen=GENERATIONSPERTRIAL))
+    # INSTANCIATE A POPULATION
+    pop = pg.population(prob, size=POPULATIONSIZE)    
+    while True:
+        # RUN THE EVOLUION
+        pop = algo.evolve(pop)
+        trials += 1
+        generations += GENERATIONSPERTRIAL
+        # EXTRACT RESULTS AND SEARCH THE BEST
+        fits, vectors = pop.get_f(), pop.get_x()
+        new_f = new_x = None
+        for index in range(len(fits)):
+            # VALID SOLUTION
+            if fits[index][1] <= 0:
+                if type(new_f) == type(None):
+                    new_f = fits[index]
+                    new_x = vectors[index]
+                elif new_f[0] > fits[index][0]:
+                    new_f = fits[index]
+                    new_x = vectors[index]    
+        
+        if type(new_f) != type(None):
+            if type(best_f) == type(None): 
+                best_f = new_f
+                best_x = new_x
+            else:
+                if best_f[0] > new_f[0]:
+                    best_f = new_f
+                    best_x = new_x
+                    nochanges = 0
+                else:
+                    nochanges += 1
+        
+        if type(best_f) != type(None):
+            print('Generations: %i '%(generations), end='')
+            print('Cost: %.2f Pressure deficit: %0.3f '%(best_f[0],best_f[1]))     
+    
+        if (perf_counter()-inittime) >= MAXTIME:
+            print('Maximum evolution time was reached.')
+            break
+        elif trials >= MAXTRIALS:
+            print('Maximum number of trials was reached.')
+            break
+        elif nochanges >= MAXNOCHANGES:
+            print('Objective function value was repeated %i times.'%(nochanges))
+            break 
+    return (best_f,best_x)           
+
--- setup.py
+++ setup.py
@@ -2,18 +2,32 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-VERSION = '0.0.0'
+DISTNAME = 'ppno'
+VERSION = '0.1.0'
 PACKAGES = find_packages()
+EXTENSIONS = []
+DESCRIPTION = 'Pressurized Pipe Network Optimizer'
 LONG_DESCRIPTION = open('README.md', encoding="utf8").read()
+AUTHOR = 'Andrés García Martínez'
+MAINTAINER_EMAIL = 'ppnoptimizer@gmail.com'
+LICENSE = 'Apache 2.0'
+URL = 'https://github.com/andresgciamtez/ppno'
 
-setup(name='ppno',
+setuptools_kwargs = {
+    'zip_safe': False,
+    'install_requires': [],
+    'scripts': [],
+    'include_package_data': True
+}
+
+setup(name=DISTNAME,
       version=VERSION,
       packages=PACKAGES,
-      description='Pressurized Pipe Network Optimizer',
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      author='Andrés García Martínez',
-      author_email='ppnoptimizer@gmail.com',
-      license='Apache 2.0',
-      url='https://github.com/andresgciamtez/ppno',
-      )
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+**setuptools_kwargs)
