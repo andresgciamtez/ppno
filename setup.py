51de4a99e54f3b8820fee7e5e55471449a4bb627
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2019-06-12 19:33:36 +0200

    new algorithm nsga2

--- setup.py
+++ setup.py
@@ -5,29 +5,16 @@ from distutils.core import Extension
 DISTNAME = 'ppno'
 VERSION = '0.1.0'
 PACKAGES = find_packages()
-EXTENSIONS = []
-DESCRIPTION = 'Pressurized Pipe Network Optimizer'
 LONG_DESCRIPTION = open('README.md', encoding="utf8").read()
-AUTHOR = 'Andrés García Martínez'
-MAINTAINER_EMAIL = 'ppnoptimizer@gmail.com'
-LICENSE = 'Apache 2.0'
-URL = 'https://github.com/andresgciamtez/ppno'
 
-setuptools_kwargs = {
-    'zip_safe': False,
-    'install_requires': [],
-    'scripts': [],
-    'include_package_data': True
-}
-
-setup(name=DISTNAME,
+setup(name='ppno',
       version=VERSION,
       packages=PACKAGES,
-      ext_modules=EXTENSIONS,
-      description=DESCRIPTION,
+      description='Pressurized Pipe Network Optimizer',
       long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      maintainer_email=MAINTAINER_EMAIL,
-      license=LICENSE,
-      url=URL,
-**setuptools_kwargs)
+      long_description_content_type='text/markdown',
+      author='Andrés García Martínez',
+      author_email='ppnoptimizer@gmail.com',
+      license='Apache 2.0',
+      url='https://github.com/andresgciamtez/ppno',
+)
