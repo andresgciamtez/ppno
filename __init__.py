e92278d99e7feeca39e6b100dcf908f9ee0221e8
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2019-11-21 13:18:24 +0100

    Restructure

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""PRESSURIZED PIPE NETWORK OPTIMIZER
-
-https://github.com/andresgciamtez/ppno (ppnoptimizer@gmail.com)
-Licensed under the Apache License 2.0. http://www.apache.org/licenses/
-"""
-
-from .ppno import Optimization
